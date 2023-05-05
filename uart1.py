7dd0417ac736a6c4eded371218aaf42d5c2a3307
Sina Glamocic <sglamocic@gmail.com>
2023-05-05 11:30:44 -0700

    Add files via upload
    Python Code / Jetson Nano / UART COMM

---
+++ uart1.py
@@ -0,0 +1,29 @@
+import serial 
+import time
+
+arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+arduino.reset_input_buffer()
+
+while True:
+    
+    #Varibales DirectionL, DirectionR, gimbalPan, gimbalTilt, LauncherMRight, LauncherMLeft, hopper, ChannelSent
+    #represented with a list inputsArduino
+    inputsArduino = [1, 2, 3, 4, 5, 6, 7, 8] 
+    
+    #Converts int values to string
+    strInputs = []
+    for i in inputsArduino:
+        strInputs.append(str(i))
+        
+    #Sends each value from the strInputs list
+    for j in strInputs:
+        arduino.write(j.encode('utf-8'))
+        
+        #tests sending data from Jetson to Arduion
+        print(strInputs)
+        
+        #Receives data from Arduino and prints in terminal
+        receivedData = arduino.readline().decode().strip()
+        print("\n\nI received from Arduino: ", receivedData, "\n\n")
+        time.sleep(0.01)
+
